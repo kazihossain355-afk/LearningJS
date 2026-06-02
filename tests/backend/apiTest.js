6ae14a558f030c9f675de02285d27d7dfdb9da14
kazihossain355-afk <kazihossain355@gmail.com>
2026-06-02 17:19:00 -0400

    Added userAPI test

---
+++ tests/backend/apiTest.js
@@ -0,0 +1,4 @@
+console.log("Open API endpoint");
+console.log("Send GET request");
+console.log("Verify status code 200");
+console.log("Verify response data");
---
+++ tests/backend/userAPI.js
@@ -0,0 +1,4 @@
+console.log("Open user API");
+console.log("Send POST request");
+console.log("Create new user");
+console.log("Verify user created");
